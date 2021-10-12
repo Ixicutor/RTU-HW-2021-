29d15d1cc345e73b781b4e48aa69a7624e665ebb
Ixicutor <90252469+Ixicutor@users.noreply.github.com>
2021-10-12 13:44:32 +0300

    Add files via upload

--- Flask_Project/api.py
+++ Flask_Project/api.py
@@ -1,6 +1,6 @@
 import flask
 from flask import request, jsonify,make_response
-
+import requests,json
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
@@ -25,34 +25,60 @@ data={
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
-@app.route('/api/resources/data/all', methods=['GET'])
+    return "<h1>Telecommunication Sofware</h1><p>This site is a prototype API for data scores.</p>"
+
+
+
+@app.route('/api/data', methods=['GET'])
 def api_all():
     return jsonify(data)
-@app.route('/api/resources/data/scores/', methods=['GET'])
-def get_scores():
-    sc=request.args.get('sc')
-    print(sc)
-    return jsonify(data['scores'][str(sc)])
 
-@app.route('/api/resources/data/scores', methods=['POST'])
+@app.route('/api/data/scores', methods=['GET'])
+def api_scores():
+    return jsonify(data["scores"])
+
+@app.route('/api/data/scores/<int:n>', methods=['GET'])
+def getn_scores(n):
+    l = list(data["scores"])[n]
+    return jsonify({list(data["scores"])[n]: (data["scores"])[l]})
+
+@app.route('/api/data/scores', methods=['POST'])
 def post_scores():
     sc=request.json['sc']
     val=request.json['value']
     data['scores'][str(sc)]=val
-    res = make_response(jsonify({"message": "data replaced"}), 200)
+    res = make_response(jsonify({"message": "data replaced"},{sc:val}), 200)
     return res
 
 
-@app.route('/api/resources/data/scores/<int>', methods=['PUT'])
-def put_scores():
+@app.route('/api/data/scores/<int:n>', methods=['PUT'])
+def put_scores(n):
     sc=request.json['sc']
     val=request.json['value']
+    l = list(data["scores"])[n]
+    data['scores'][str(sc)]=(data["scores"]).pop(l)
     data['scores'][str(sc)]=val
-    res = make_response(jsonify({"message": "data updated"}), 200)
+    res = make_response(jsonify({"message": "data updated"},{sc,val}), 200)
     return res
 
 
+@app.route('/api/data/scores/<int:n>', methods=['PATCH'])
+def patch_scores(n):
+    val=request.json['value']
+    l = list(data["scores"])[n]
+    data['scores'][l]=val
+    res = make_response(jsonify({list(data["scores"])[n]: val}), 200)
+    return res
+
+
+
+
+
+@app.route('/api/data/scores/<int:n>', methods=['DELETE'])
+def del_score(n):
+    l = list(data["scores"])[n]
+    (data["scores"]).pop(l)
+    return make_response(jsonify({"message": "data deleted"}), 200)
 
 
 app.run()
